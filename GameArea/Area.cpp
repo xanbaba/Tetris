23436461085fb5420fda0ce667c141d2a9486752
XanbabaF <xanbabaf@gmail.com>
2023-10-07 22:46:22 +0400

    Creating area functions

--- GameArea/Area.cpp
+++ GameArea/Area.cpp
@@ -1,1 +1,19 @@
 ﻿#include "Area.h"
+
+area create_area(int width, int height)
+{
+    area game_area;
+    game_area.width = width;
+    game_area.height = height;
+    game_area.area_array = create_array_2d<char>(height);
+
+    for (int i = 0; i < height; ++i)
+    {
+        auto row = create_array<char>(width);
+        row.size = width;
+        append_row(game_area.area_array, row);
+        
+    }
+
+    return game_area;
+}
--- GameArea/Area.h
+++ GameArea/Area.h
@@ -1,1 +1,11 @@
 ﻿#pragma once
+#include "../Array/Array2D.h"
+
+struct area
+{
+    Array2D<char> area_array;
+    int width;
+    int height;
+};
+
+area create_area(int width, int height);
