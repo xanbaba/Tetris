0c4af6a5f5d3006a0a5171195fdc45f886e3d93c
XanbabaF <xanbabaf@gmail.com>
2023-10-07 22:47:33 +0400

    starting game

--- Tetris.cpp
+++ Tetris.cpp
@@ -1,8 +1,10 @@
-#include <iostream>
-#include "GameArea/Area.h"
+#include "Game/Game.h"
 
 int main(int argc, char* argv[])
 {
-    
+    // while (true)
+    {
+        start_game(area_size::width, area_size::height);
+    }
     return 0;
 }
